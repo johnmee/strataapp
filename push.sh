205672c2b3ba26394762bfbe6452ca1ebe81b684
John Mee <gitcommit@johnmee.com>
2026-04-07 10:15:48 +1000

    Serve the blog from nginx and rebuild it on deploy.

--- push.sh
+++ push.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-git push "$@" && ssh norry 'cd /home/john/strataapp/ && git pull && git submoddule update --remote && sudo systemctl reload nginx'
+git push "$@" && ssh norry 'cd /home/john/strataapp/ && git pull && git submodule update --remote && cd blog && hugo && sudo systemctl reload nginx'
